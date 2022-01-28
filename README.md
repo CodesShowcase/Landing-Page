5def7dc6a3a500f818664f25aca6e9e47671f59e
nicks-showcase <nhaumer@mail.com>
2022-01-28 15:36:09 +0100

    Final

---
+++ README.md
@@ -0,0 +1,13 @@
+# Landing Page Project
+
+## Table of Contents
+
+* [Documentation](#documentation)
+
+## Documentation
+
+A fourth section was added to the three section starting project. For this the third section was copied in index.html and adjustments were made, that it is now section four.
+
+In addition some minor tweeks were made to the styles.css file. Now e.g. the active section (visible section) is highlighted.
+
+Major changes/additions were made to the Javascript. There is a dynamic navbar which automatically detects all sections and update the navbar. Also the active (visible) section will be highlighted and a ScrollToTopButton was implemented. As an additional feature the navbar will autohide if there is no scrolling activity.
---
+++ js/app.js
@@ -0,0 +1,85 @@
+/**
+ * 
+ * Manipulating the DOM exercise.
+ * Exercise programmatically builds navigation,
+ * scrolls to anchors from navigation,
+ * and highlights section in viewport upon scrolling.
+ * 
+ * Dependencies: None
+ * 
+ * JS Version: ES2015/ES6
+ * 
+ * JS Standard: ESlint
+ * 
+*/
+
+/**
+ * Comments should be present at the beginning of each procedure and class.
+ * Great to have comments before crucial code sections within the procedure.
+*/
+
+/**
+ * Define Global Variables
+ * 
+*/
+const navbar    = document.getElementById("navbar__list");
+const sections  = document.querySelectorAll("section");
+
+// build the nav
+function nav_builder () {
+
+  let navEntries = "";
+
+  sections.forEach(entry => {
+    let section = entry.id;
+    section = section[0].toUpperCase() + section.slice(1);
+    navEntries += `<li><a href=\"index.html#${entry.id}" id="menuitem-${entry.id}">${entry.getAttribute('data-nav')}</a></li>`;
+   });
+ 
+  navEntries = navEntries.replace(/<\/li><li>/g, '</li> | <li>');
+  $('#navbar__list').append($(navEntries));
+
+}
+
+// set visible section & nav entry active
+function toggleActive() {
+
+  sections.forEach(section => {
+
+    const rect = section.getBoundingClientRect();
+    const active = rect.top >= 0 && rect.left >= 0 && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && rect.right <= (window.innerWidth || document.documentElement.clientWidth);
+    const menuitem = "menuitem-" + section.id;
+    active ? section.classList.add('your-active-class') : section.classList.remove('your-active-class');
+    active ? document.getElementById(menuitem).classList.add('active-menuitem') : document.getElementById(menuitem).classList.remove('active-menuitem');
+  
+  });
+
+}
+
+// does what it says, it autohides the navigation menu
+function autoHide() {
+  $('.navbar__menu').show();
+  $('.navbar__menu').delay(9000).fadeOut(1000);
+}
+
+function topScroll() {
+  window.scrollTo({top: 0, behavior: 'smooth'});
+
+  //Does also work, but smooth is not working...
+  //$('html,body').animate({scrollTop: $(window).scrollTop(0)}, 'slow');
+
+}
+
+function triggerBtn() {
+  if (document.body.scrollTop > 80) {
+    $('.navbar').css('opacity', '1');
+  } else {
+    $('#top-scroll-btn').css('opacity', '0')
+};
+}
+
+$(document).ready(() => { nav_builder(); autoHide(); });
+
+document.getElementById("top-scroll-btn").addEventListener("click", topScroll);
+
+document.addEventListener("scroll", () => { toggleActive(); autoHide(); triggerBtn(); });
